094cd74a87146618a73420562a84ae2eda1ee0d9
Nash P <nashakpd@gmail.com>
2022-10-24 12:20:28 +0100

    Added command line arguments

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from yt_dlp import YoutubeDL as ydl
 import ffmpeg
+import sys
 
 
 test_url = "https://youtu.be/FtutLA63Cp8"  # Bad Apple, because yes
-test_start_timestamp = 6
-test_end_timestamp = 36
+test_start = 6
+test_end = 36
 
 # Extract Url
 def get_url(url) -> str:
@@ -24,17 +25,33 @@ def get_url(url) -> str:
     return audio_direct_url
 
 
-def snip_url(url):
+def get_secs(time_str) -> int:  # hehe
+    h, m, s = time_str.split(":")
+    secs = (int(h) * 3600) + (int(m) * 60) + int(s)
+    return secs
+
+
+def snip_url(url, start, end):
     input = ffmpeg.input(url)
     audio = input.audio
     # stream = ffmpeg.trim(audio, 60)
-    out = ffmpeg.output(
-        audio, "out.mp4", ss=test_start_timestamp, to=test_end_timestamp
-    )
+    out = ffmpeg.output(audio, "out.mp4", ss=start, to=end)
     return ffmpeg.run(out)
+
+
+if __name__ != "__main__":
+    stream_url = get_url(test_url)
+    snip_url(stream_url, test_start, test_end)
 
 
 if __name__ == "__main__":
     print("LOG: Running main")
-    stream_url = get_url(test_url)
-    snip_url(stream_url)
+    args = sys.argv
+    if len(args) < 4:
+        print("ERROR: Not enough arguments")
+    else:
+        self_arg, target_url, start_time, end_time = args
+        start_time = get_secs(start_time)
+        end_time = get_secs(end_time)
+        stream_url = get_url(target_url)
+        snip_url(stream_url, start_time, end_time)
