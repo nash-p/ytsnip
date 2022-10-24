4d5147f606aaf5c6a88ab876b1cc64e2ce91c9ab
Nash P <nashakpd@gmail.com>
2022-10-24 12:53:59 +0100

    Added title extraction

--- main.py
+++ main.py
@@ -7,12 +7,21 @@ test_url = "https://youtu.be/FtutLA63Cp8"  # Bad Apple, because yes
 test_start = 6
 test_end = 36
 
+
+def get_secs(time_str) -> int:  # hehe
+    h, m, s = time_str.split(":")
+    secs = (int(h) * 3600) + (int(m) * 60) + int(s)
+    return secs
+
+
 # Extract Url
-def get_url(url) -> str:
+def get_url(url):
 
     ydl_opts = {}
     raw = ydl(ydl_opts).extract_info(url, download=False)  # dict of lists
     info = ydl.sanitize_info(raw)
+
+    title = info["title"].strip() + ".mp4"
 
     formats = info["formats"]  # list of dicts
     audio_direct_url = ""
@@ -22,36 +31,32 @@ def get_url(url) -> str:
             break
 
     # return r["formats"][-1]["url"]
-    return audio_direct_url
+    return audio_direct_url, title
 
 
-def get_secs(time_str) -> int:  # hehe
-    h, m, s = time_str.split(":")
-    secs = (int(h) * 3600) + (int(m) * 60) + int(s)
-    return secs
-
-
-def snip_url(url, start, end):
+def snip_url(url, title, start, end):
     input = ffmpeg.input(url)
     audio = input.audio
     # stream = ffmpeg.trim(audio, 60)
-    out = ffmpeg.output(audio, "out.mp4", ss=start, to=end)
+    out = ffmpeg.output(audio, ("out/" + title), ss=start, to=end)
     return ffmpeg.run(out)
 
 
 if __name__ != "__main__":
-    stream_url = get_url(test_url)
-    snip_url(stream_url, test_start, test_end)
+    stream_url, title = get_url(test_url)
+    snip_url(stream_url, title, test_start, test_end)
 
 
 if __name__ == "__main__":
     print("LOG: Running main")
     args = sys.argv
+
     if len(args) < 4:
         print("ERROR: Not enough arguments")
+
     else:
         self_arg, target_url, start_time, end_time = args
         start_time = get_secs(start_time)
         end_time = get_secs(end_time)
-        stream_url = get_url(target_url)
-        snip_url(stream_url, start_time, end_time)
+        stream_url, title = get_url(target_url)
+        snip_url(stream_url, title, start_time, end_time)
